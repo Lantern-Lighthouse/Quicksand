97f6b55c15d4ee6f14f3cab5c03dbac94343b604
Písek Pískovec <pisekpiskovec@gmail.com>
2025-10-25 21:32:49 +0200

    feat: Sanitize server url

--- index.php
+++ index.php
@@ -4,4 +4,8 @@ require('./vendor/autoload.php');
 $base = \Base::instance();
 $base->config('./app/Configs/config.ini');
 
+if(str_ends_with($base->get("QS.ATHEJA_SERVER_URL"), '/')) {
+    $base->set("QS.ATHEJA_SERVER_URL", substr($base->get("QS.ATHEJA_SERVER_URL"), 0, strlen($base->get("QS.ATHEJA_SERVER_URL")) - 1));
+}
+
 $base->run();
